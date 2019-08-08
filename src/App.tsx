a4a5ce0c67b41b2d1dc43f115024859a6e90d5d0
Hai Ha Nguyen <haihanguyen@Hais-MacBook-Pro.local>
2019-08-08 18:39:37 +0200

    adding an ITodo interface, new adding methods

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,61 @@
 import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
+import { string } from "prop-types";
+
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
 
 const App: React.FC = () => {
+  //initialiazing State using Hooks
   const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   type FormElem = React.FormEvent<HTMLFormElement>;
 
-  const handleSubmit = (e: FormElem):void => {
+  const handleSubmit = (e: FormElem): void => {
     e.preventDefault();
-    setValue('');
-  }
-  
+    addTodo(value);
+    // cleaning the State after the todo is added
+    setValue("");
+  };
+
+  const addTodo = (text: string) => {
+    const newTodos: ITodo[] = [...todos, { text, complete: false }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = (index: number) => {
+    const newTodos: ITodo[] = [...todos];
+    newTodos[index].complete = !newTodos[index].complete;
+    setTodos(newTodos);
+  };
+
+  console.log(todos);
+
   return (
     <Fragment>
       <h1>Hello</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" value={value} onChange={e => setValue(e.target.value)} required />
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
         <button type="submit">Add todo</button>
+        <section>
+          {todos.map((todo: ITodo, index: number) => (
+            <Fragment key={index}>
+              <div>{todo.text}</div>
+              <button type="button" onClick={() => completeTodo(index)}>
+                {todo.complete ? "Incomplete" : "Complete"}
+              </button>
+            </Fragment>
+          ))}
+        </section>
       </form>
     </Fragment>
   );
